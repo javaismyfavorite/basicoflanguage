d3aeda0e9f1f37e839775dd3c2a2001ad7fdcd11
Me and All <79387572+javaismyfavorite@users.noreply.github.com>
2021-11-21 11:17:22 +0700

    Menampilkan teks di layar

---
+++ Main.java
@@ -0,0 +1,10 @@
+//kita buat kelas Main
+public class Main
+{
+//kita buat fungsi utama
+public static void main(String[] args)
+{
+//kita ingin menampilkan teks
+System.out.println("Hello, nama saya Budi!");
+}
+}
